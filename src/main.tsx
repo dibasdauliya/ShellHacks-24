37014da54a182f03962a92de46c7c4ad02b0ff63
dibasdauliya <dibasdauliya@gmail.com>
2024-09-28 00:39:11 -0400

    add: react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./route/App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Test from "./route/Test.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/route/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Button } from "../components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl">fddsa</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
